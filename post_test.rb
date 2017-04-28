e56aace79d7ae37ce836f619f4aa2083ef9a6be1
Shohei Aoki <shoaok@gmail.com>
2017-04-28 14:50:06 +0900

    update post test

--- post_test.rb
+++ post_test.rb
@@ -1,3 +1,5 @@
 format = "%Y-%m-%d %H:%M:%S"
-cmd = 'curl -H "Accept: application/json" -H "Content-type: application/json" http://localhost:3000/sensors/1/measured_results -X POST -d "{\"data\":[{\"raw_value\":'+rand.to_s+',\"measured_at\":\"'+Time.now.strftime(format)+'\"}]}"'
+baseurl = "http://localhost:3000"
+url = baseurl + "/sensors/1/measured_results" 
+cmd = 'curl -H "Accept: application/json" -H "Content-type: application/json" '+url+' -X POST -d "{\"data\":[{\"raw_value\":'+rand.to_s+',\"measured_at\":\"'+Time.now.strftime(format)+'\"}]}"'
 system(cmd)
